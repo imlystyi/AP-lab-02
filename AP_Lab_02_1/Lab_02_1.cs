99696175a9652c3a856b4718964a304972cdc215
imlystyi <vladte3c@gmail.com>
2022-10-05 16:44:34 +0300

    comments commit

--- AP_Lab_02_1/Lab_02_1.cs
+++ AP_Lab_02_1/Lab_02_1.cs
@@ -13,12 +13,14 @@ namespace AP_Lab_02_1
         {
             double ConvertDegToRad(double angleToConvert) => (Math.PI / 180) * angleToConvert;
 
+            // Input part
             Console.Write("alpha: ");
             double alpha = ConvertDegToRad(Double.Parse(Console.ReadLine()));
 
             double z1 = (Math.Sin(alpha) + Math.Sin(5 * alpha) - Math.Sin(3 * alpha)) / (Math.Cos(alpha) - Math.Cos(3 * alpha) + Math.Cos(5 * alpha)),
                 z2 = Math.Tan(3 * alpha);
 
+            // Output part
             Console.WriteLine($"z1 = {z1};\nz2 = {z2};\n");
 
             Console.ReadLine();
